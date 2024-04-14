5795e56fca670e15ba6386807c7f135dc5a16e11
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2024-04-14 12:12:29 -0300

    test: add EntityTest.php

---
+++ src/domain/@shared/entity/EntityTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Domain\Entity\Entity;
+use App\Domain\Notification\Notification;
+use PHPUnit\Framework\TestCase;
+
+class MyTestEntity extends Entity
+{
+  public function __construct(string $id)
+  {
+    parent::__construct($id);
+  }
+}
+
+final class EntityTest extends TestCase
+{
+  public function testCanBeExtended(): void
+  {
+    $entity = new MyTestEntity('test-id');
+
+    $this->assertInstanceOf(MyTestEntity::class, $entity);
+    $this->assertEquals('test-id', $entity->id);
+    $this->assertInstanceOf(Notification::class, $entity->notification);
+  }
+}
